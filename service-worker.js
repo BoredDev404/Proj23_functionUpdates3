17762045aaba6439555314cde03c84a91999ef46
BoredDev404 <rihazrizvi@gmail.com>
2025-11-20 20:32:40 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+// service-worker.js
+const CACHE_NAME = 'life-tracker-v7.0';
+const urlsToCache = [
+  '/Proj23_functionUpdates2/',
+  '/Proj23_functionUpdates2/index.html',
+  '/Proj23_functionUpdates2/style.css',
+  '/Proj23_functionUpdates2/app.js',
+  '/Proj23_functionUpdates2/db.js',
+  '/Proj23_functionUpdates2/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/dexie@3.2.4/dist/dexie.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
